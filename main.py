3c639fc19ac7caa9a297093da1ac92d2f6fd45a3
Christian Armgarth <christian.armgarth@gmail.com>
2025-09-03 15:43:47 +0200

    Removed port from dockerfile and debug=true from main.py

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ def api_status():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
