69d0e5a1fd632298c67446d6f7f10871b5da68a2
John Kinell <johnkinell@gmail.com>
2025-09-05 22:49:30 +0200

    NASA Pipeline first try

--- nasa_pipeline/main.py
+++ nasa_pipeline/main.py
@@ -1,9 +1,7 @@
 import requests
-import json
 import os
 from datetime import datetime, timedelta, date
 from google.cloud import bigquery
-from google.cloud.exceptions import NotFound
 import pandas as pd
 import logging
 
@@ -15,7 +13,8 @@ logger = logging.getLogger(__name__)
 
 TODAY = date.today().strftime("%Y-%m-%d")
 YESTERDAY = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
-NOW = datetime.now().strftime("%Y-%m-%d")
+NOW = datetime.now()
+API_KEY = os.environ("NASA_API")
 
 
 class DataPipeline:
@@ -29,17 +28,84 @@ class DataPipeline:
         logger.info("Pipeline initialized for NASA data collection.")
 
     def fetch_data(self):
-        """ "Fetch data from NASA's API"""
+        """Fetch data from NASA's API"""
+
+        url = "https://api.nasa.gov/neo/rest/v1/feed"
+        params = {
+            "start_date": YESTERDAY,
+            "end_date": YESTERDAY,
+            "api_key": API_KEY,
+        }
+
         try:
             logger.info("Fetching data from NASA API...")
-            response = requests.get()
-        except:
-            pass
+            response = requests.get(url, params=params, timeout=10)
+            response.raise_for_status()
 
+            data = response.json()
+            near_earth_objects = data.get("near_earth_objects", {}).get(YESTERDAY, [])
 
-def main():
-    print("Hello from nasa-pipeline!")
+            for record in near_earth_objects:
+                record["fetched_at"] = NOW.isoformat()
+
+            logger.info(f"Successfully fetched {len(near_earth_objects)} record(s)")
+            return near_earth_objects
+        except requests.RequestException as e:
+            logger.error(f"Error fetching data: {e}")
+            raise
+
+    def write_to_bigquery(self, data):
+        """Write data to nasa_table in BigQuery"""
+        if not data:
+            logger.info("No data to write to BigQuery.")
+            return
+
+        table_ref = self.client.dataset(self.dataset_id).table(self.table_id)
+
+        try:
+            df = pd.DataFrame(data)
+            logger.info(f"Prepared {len(df)} record(s) for storage")
+
+            job_config = bigquery.LoadJobConfig(
+                write_disposition="WRITE_APPEND",
+                autodetect=False,  # Set to True to handle schema detection
+            )
+
+            logger.info("Writing data to BigQuery")
+            job = self.client.load_table_from_dataframe(
+                df,
+                table_ref,
+                job_config=job_config,
+            )
+
+            job.result()
+
+            table = self.client.get_table(table_ref)
+            logger.info(f"Write complete! Table now has {table.num_rows} total rows")
+
+        except Exception as e:
+            logger.error(f"Error uploading to BigQuery: {e}")
+            raise
+
+    def run_pipeline(self):
+        """Execute the complete data pipeline"""
+        try:
+            logger.info("=" * 50)
+            logger.info("Staring NASA Data Pipeline")
+            logger.info("=" * 50)
+
+            data = self.fetch_data()
+
+            self.write_to_bigquery(data)
+
+            logger.info("=" * 50)
+            logger.info("Pipeline completed Successfully!")
+            logger.info("=" * 50)
+        except Exception as e:
+            logger.error(f"Pipeline failed: {e}")
+            raise
 
 
 if __name__ == "__main__":
-    main()
+    pipeline = DataPipeline()
+    pipeline.run_pipeline()
