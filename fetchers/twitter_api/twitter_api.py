66197733fce6b7623d72b4d94349810d2e32553c
Christian Armgarth <christian.armgarth@gmail.com>
2025-09-10 12:12:05 +0200

    Sync all time-elements to Stockholm time

--- fetchers/twitter_api/twitter_api.py
+++ fetchers/twitter_api/twitter_api.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import os
-from datetime import UTC, datetime, timezone, timedelta
+from datetime import datetime, timedelta, timezone
 
 import tweepy
 from dotenv import load_dotenv
